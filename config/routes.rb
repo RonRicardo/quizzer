69f1890a82147e73688a754a950757ba836c65d0
RonRicardo <roniecericardo@gmail.com>
2019-04-06 14:07:19 -0400

    refactors routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "home#index"
+  devise_for :users
+  post 'questions/:id/:answer_id', to: "questions#create_user_answer", as: "create_user_answer"
+
+  resources :questions, only: [:index, :show]
+
+  resources :users do
+    get 'questions', to: "questions#user_questions", as: "questions"
+    resources :questions, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
