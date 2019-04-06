60a4273bb8af10db4f1f06f7accb335062962a71
RonRicardo <roniecericardo@gmail.com>
2019-04-06 13:09:06 -0400

    adds routes and controller for Devise, silences some generators

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,13 @@ module Quizzer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Have generators skip test framework, assets, helpers and stylesheets
+    config.generators do |g|
+      g.test_framework  false
+      g.assets  false
+      g.helper false
+      g.stylesheets false
+    end
   end
 end
