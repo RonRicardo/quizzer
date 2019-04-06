b72bb52b51170bb7c36b4726d368189774730c54
RonRicardo <roniecericardo@gmail.com>
2019-04-06 13:40:57 -0400

    adds validations and nested attributes for Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :answers
+  validates :title, presence: true
+  accepts_nested_attributes_for :answers, reject_if: proc { |attributes| attributes['content'].blank? }
 end
